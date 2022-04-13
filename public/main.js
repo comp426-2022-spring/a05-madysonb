7c916f2bb9dddc150f1b68a47b725db5cb91df73
Madyson Barber <madysonb@live.unc.edu>
2022-04-13 13:07:50 -0400

    added everything from w11

--- public/main.js
+++ public/main.js
@@ -1,8 +1,22 @@
+// Dependencies
+const cors = require('cors') // Add cors dependency
+app.use(cors()) // Set up cors middleware on all endpoints
+    
+app.use(express.json()) // Allow JSON body messages on all endpoints
+
 // Focus div based on nav button click
 
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
+app.post('/app/flip/coins/', (req, res, next) => {
+    const flips = coinFlips(req.body.number)
+    const count = countFlips(flips)
+    res.status(200).json({"raw":flips,"summary":count})
+})
+// Serve static HTML files
+app.use(express.static('./index.html'))
+
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
