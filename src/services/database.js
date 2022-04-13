5ce7fbdb140e85520442514f6989858d0c2eedbf
Madyson Barber <madysonb@live.unc.edu>
2022-04-13 13:11:56 -0400

    moved a04 database.js

--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,26 @@
-// Put your database code here
+// Require better-sqlite.
+const Database = require('better-sqlite3');
+const db = new Database('log.db');
+
+
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+let row = stmt.get();
+
+// init db
+if (row === undefined) {
+    console.log('Log database is empty. Creating log database...')
+
+    const sqlInit = `
+        CREATE TABLE accesslog ( id INTEGER NOT NULL PRIMARY KEY, 
+            remoteaddr TEXT, remoteuser TEXT, time INTEGER, 
+            method TEXT, url TEXT, protocol TEXT, 
+            httpversion TEXT, status INTEGER, 
+            referer TEXT, useragent TEXT);
+    `;
+
+    db.exec(sqlInit)
+} else {
+    console.log("Database exists");
+}
+
+module.exports = db;
