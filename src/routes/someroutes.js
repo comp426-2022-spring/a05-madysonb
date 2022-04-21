1854227d1123b1252ea473b931de08db6415ed94
Madyson Barber <madysonb@live.unc.edu>
2022-04-20 20:08:05 -0400

    one flip

--- src/routes/someroutes.js
+++ src/routes/someroutes.js
@@ -1,1 +1,14 @@
 // Route (endpoint) definitions go in this directory
+
+const express = require("express");
+
+flipFuncs = require(' path to coin functions')
+
+const routes = express.Router()
+
+// single flip
+routes.route('/app/flip/').get(function (req, res, next) {
+    const flip = flipFuncs.coinFlip()
+    res.status(200).json({'flip': flip})
+
+})
