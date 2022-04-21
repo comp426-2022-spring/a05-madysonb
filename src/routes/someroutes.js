94dbfc949099a25efa8aaaa9ab56e7ac012c34bd
Madyson Barber <madysonb@live.unc.edu>
2022-04-20 20:22:10 -0400

    move routes to someroutes

--- src/routes/someroutes.js
+++ src/routes/someroutes.js
@@ -2,13 +2,29 @@
 
 const express = require("express");
 
-flipFuncs = require(' path to coin functions')
+flipFuncs = require('../utils/utilities.js')
 
-const routes = express.Router()
+const routesF = express.Router()
 
 // single flip
-routes.route('/app/flip/').get(function (req, res, next) {
+routesF.route('/app/flip/').get(function (req, res, next) {
     const flip = flipFuncs.coinFlip()
     res.status(200).json({'flip': flip})
 
 })
+
+// multi flips
+routesF.route('/app/flips/coins/').post(function (req, res, next) {
+    const flip = flipFuncs.countFlips(flipFuncs.coinFlips(req.body.number))
+
+    res.status(200).json(flip)
+})
+
+// guess flip
+routesF.route('/app/flip/call/').post(function (req, res, next) {
+    const flip = flipFuncs.flipACoin(req.body.guess)
+    res.status(200).json(flip)
+})
+
+
+module.exports = routesF
