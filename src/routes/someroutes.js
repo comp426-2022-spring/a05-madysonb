fac1feae44ba09052095d0b21acd7872dabb65fd
Madyson Barber <madysonb@live.unc.edu>
2022-04-28 09:49:36 -0400

    added back old routes

--- src/routes/someroutes.js
+++ src/routes/someroutes.js
@@ -27,11 +27,21 @@ routesF.route('/app/flips/coins/').post(function (req, res, next) {
     
 })
 
+routesF.route('/app/flips/:number').get(function (req, res) {
+    const flips = flipFuncs.coinFlips(req.params.number)
+    const count = flipFuncs.countFlips(flips)
+    res.status(200).json({ "raw": flips, "summary": count })
+});
+
 // guess flip
 routesF.route('/app/flip/call/').post(function (req, res, next) {
     const flip = flipFuncs.flipACoin(req.body.guess)
     res.status(200).json(flip)
 })
 
+routesF.route('/app/flip/call/:guess(heads|tails)').get(function (req, res) {
+    const game = flipFuncs.flipACoin(req.params.guess)
+    res.status(200).json(game)
+})
 
 module.exports = routesF
